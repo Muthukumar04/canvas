1f35127e1681ec5eb135bff9cd576217684ef44b
Muthukumar M <muthukumar@kissflow.com>
2023-04-09 11:55:01 +0530

    moving line and rect

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import "./App.css";
 const generator = rough.generator();
 
 const actions = {
-  drawing: "d",
-  selection: "s",
-  none: "n",
+  drawing: "drawing",
+  selection: "selection",
+  resizing: "resizing",
+  moving: "moving",
+  none: "none",
 };
 const shapes = {
-  line: "l",
-  rectangle: "r",
-  none: "n",
+  line: "line",
+  rectangle: "rectangle",
+  none: "none",
 };
 
-function createElement(x1, y1, x2, y2, type) {
+function createElement(x1, y1, x2, y2, type, id) {
   let element = null;
   switch (type) {
     case shapes.line: {
@@ -31,7 +33,67 @@ function createElement(x1, y1, x2, y2, type) {
     default:
       throw new Error("Invalid shape type");
   }
-  return { x1, y1, x2, y2, element };
+  return { x1, y1, x2, y2, element, type, id };
+}
+
+function distanceBetweenPoints(A, B) {
+  return Math.sqrt(Math.pow(A.x - B.x, 2) + Math.pow(A.y - B.y, 2));
+}
+function liesWithinElement(x, y, element) {
+  switch (element.type) {
+    case shapes.line: {
+      const ac = distanceBetweenPoints(
+        { x, y },
+        { x: element.x1, y: element.y1 }
+      );
+      const ab = distanceBetweenPoints(
+        { x: element.x1, y: element.y1 },
+        { x: element.x2, y: element.y2 }
+      );
+      const cb = distanceBetweenPoints(
+        { x, y },
+        { x: element.x2, y: element.y2 }
+      );
+      return ac + cb <= ab + 10;
+    }
+    case shapes.rectangle: {
+      const minX = Math.min(element.x1, element.x2);
+      const minY = Math.min(element.y1, element.y2);
+      const maxX = Math.max(element.x1, element.x2);
+      const maxY = Math.max(element.y1, element.y2);
+      const isInside = x >= minX && x <= maxX && y >= minY && y <= maxY;
+      return isInside;
+    }
+    default:
+      throw new Error(`${type} not exist`);
+  }
+}
+
+//returns {x1,x2,y1,y2,element}
+function updateElement(x, y, element, mode) {
+  let updatedElement = null;
+  let { offsetX, offsetY } = element;
+  if (actions.moving === mode) {
+    switch (element.type) {
+      case shapes.line:
+      case shapes.rectangle: {
+        const width = Math.abs(element.x1 - element.x2);
+        const height = Math.abs(element.y1 - element.y2);
+        const x1 = x - offsetX,
+          y1 = y - offsetY,
+          x2 = width + x1,
+          y2 = height + y1;
+        updatedElement =
+          shapes.rectangle === element.type
+            ? generator.rectangle(x1, y1, width, height)
+            : generator.line(x1, y1, x2, y2);
+        return { element: updatedElement, x1, x2, y1, y2 };
+      }
+      default:
+        throw new Error(`Invalid type - ${element.type} for ${mode}`);
+    }
+  } else if (actions.resizing === mode) {
+  }
 }
 
 const defaultAction = { action: actions.none };
@@ -64,15 +126,30 @@ function App() {
   }, [elements]);
 
   function mouseDown(e) {
-    if (action.action === actions.drawing && action.shape) {
-      const { clientX, clientY } = e;
+    const { clientX, clientY } = e;
+    if (action.action === actions.selection) {
+      const selectedElement = elements.find((element) =>
+        liesWithinElement(clientX, clientY, element)
+      );
+
+      if (selectedElement) {
+        const offsetX = clientX - selectedElement.x1,
+          offsetY = clientY - selectedElement.y1;
+
+        setSelectedElement({ ...selectedElement, offsetX, offsetY });
+        // debugger;
+      }
+    } else if (action.action === actions.drawing && action.shape) {
+      const id = elements.length;
       const createdElement = createElement(
         clientX,
         clientY,
         clientX,
         clientY,
-        action.shape
+        action.shape,
+        id
       );
+
       setElements((p) => [...p, createdElement]);
       setSelectedElement(createdElement);
     }
@@ -81,19 +158,68 @@ function App() {
   function mouseMove(e) {
     const { clientX, clientY } = e;
     if (action.action === actions.drawing && action.shape && selectedElement) {
-      const x2 = clientX,
-        y2 = clientY;
+      //drawing new element
       const index = elements.length - 1;
       const currentElement = elements[index];
-      const { x1, y1 } = currentElement;
-      const createdElement = createElement(x1, y1, x2, y2, action.shape);
+      const { x1, y1, id, type } = currentElement;
+      const createdElement = createElement(x1, y1, clientX, clientY, type, id);
       const updatedElements = [...elements];
       updatedElements[index] = createdElement;
       setElements(updatedElements);
+    } else if (action.action === actions.selection && selectedElement) {
+      //selecting a element
+      console.log(selectedElement);
+      let updatedElement = updateElement(
+        clientX,
+        clientY,
+        selectedElement,
+        actions.moving
+      );
+      updatedElement = { ...selectedElement, ...updatedElement };
+
+      setElements((p) =>
+        p.map((element) =>
+          element.id === updatedElement.id ? updatedElement : element
+        )
+      );
+      // setSelectedElement(updatedElement);
+    }
+  }
+
+  function updateCoordinates(element) {
+    switch (element.type) {
+      case shapes.line:
+      case shapes.rectangle: {
+        let { x1, x2, y1, y2 } = element;
+        console.log(`((${x1}, ${y1} ),( ${x2}, ${y2}))`);
+        if (x1 > x2 || y1 > y2) {
+          let t = x1;
+          x1 = x2;
+          x2 = t;
+          t = y1;
+          y1 = y2;
+          y2 = t;
+        }
+        console.log(`((${x1}, ${y1} ),( ${x2}, ${y2}))`);
+        return createElement(x1, y1, x2, y2, element.type, element.id);
+      }
+      default:
+        throw new Error("Invalid type" + element.type);
     }
   }
 
   function mouseUp() {
+    if (selectedElement) {
+      const updatedElement = updateCoordinates(elements[elements.length - 1]);
+      console.log(updatedElement, selectedElement);
+      setElements((p) =>
+        p.map((element) =>
+          element.id === updatedElement.id
+            ? { ...selectedElement, ...updatedElement }
+            : element
+        )
+      );
+    }
     setSelectedElement(null);
   }
 
@@ -112,7 +238,7 @@ function App() {
       >
         <button
           onClick={() => {
-            dispatch(actions.selection);
+            dispatch({ type: actions.selection });
           }}
         >
           Selection
